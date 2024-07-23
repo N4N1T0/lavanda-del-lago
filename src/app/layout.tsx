e248de55f854324275c98504dc8940870d86ee1a
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-07-23 13:53:00 +0200

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,28 +20,7 @@ import { esES } from '@clerk/localizations'
 
 // Local Fonts Configuration
 const lemon = localFont({
-	src: [
-		{
-			path: './lemon/LemonMilkProUltraLight.woff2',
-			weight: '100',
-			style: 'light',
-		},
-		{
-			path: './lemon/LemonMilkProRegular.woff2',
-			weight: '400',
-			style: 'normal',
-		},
-		{
-			path: './lemon/LemonMilkProMedium.woff2',
-			weight: '500',
-			style: 'medium',
-		},
-		{
-			path: './lemon/LemonMilkProBold.woff2',
-			weight: '700',
-			style: 'bold',
-		},
-	],
+	src: './lemon/LemonMilkProRegular.woff2',
 	variable: '--font-lemon',
 })
 
