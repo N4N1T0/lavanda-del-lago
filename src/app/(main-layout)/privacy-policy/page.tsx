3255a72d6424eb25bca8b5c569e4298f3d1ef3e9
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-03 22:34:10 +0200

    feat: New Policies Page

---
+++ src/app/(main-layout)/privacy-policy/page.tsx
@@ -0,0 +1,37 @@
+// React Imports
+import React from 'react'
+
+// Queries Imports
+import { sanityClient } from '@sanity-studio/lib/client'
+import { privacyPolicy } from '@/lib/queries'
+
+// Components Imports
+import { ServerFetchError } from '@/components/shared/server-fetch-error'
+import { PortableText } from 'next-sanity'
+
+// Types Imports
+import { Policies } from '@/types'
+
+/**
+ * Fetches the cookies policy from the Sanity client and renders it.
+ *
+ * @return {Promise<JSX.Element>} The JSX element representing the cookies policy.
+ * @throws {Error} If there is an error fetching the policy.
+ */
+const PrivacyPoliciesPage = async (): Promise<JSX.Element> => {
+  try {
+    const response: Policies = await sanityClient.fetch(privacyPolicy)
+    return (
+      <main>
+        <h1>{response.title}</h1>
+        <div className='w-full md:w-3/4 text-left space-y-7'>
+						<PortableText value={response.content} />
+				</div>
+      </main>
+    )
+  } catch (error) {
+    return <ServerFetchError error={error} />
+  }
+}
+
+export default PrivacyPoliciesPage
