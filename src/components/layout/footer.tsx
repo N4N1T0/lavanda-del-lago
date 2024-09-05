30db4e1baceed4c6773d2c5f97dfa7800567a9d8
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-05 21:33:39 +0200

    feat: Add Sanity Client Fetching

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -11,47 +11,62 @@ import { footerLinks, socialLinks, badges } from '@/constants/site-data'
 // External Libraries Imports
 import { v4 as uuidv4 } from 'uuid'
 
-/**
- * Renders a footer component with a logo, social links, services list, contact information, and copyright notice.
- *
- * @return {JSX.Element} The footer component.
- */
-const Footer = (): JSX.Element => {
+// Queries Imports
+import { sanityClient } from '@sanity-studio/lib/client'
+import { footer } from '@/lib/queries'
+
+// Types Imports
+import type { Footer as FooterProps } from '@/types'
+
+const Footer = async (): Promise<JSX.Element> => {
+	const response: FooterProps = await sanityClient.fetch(footer)
+
+	const findSocialMedia = (name: string) => {
+		return socialLinks.find((item) => item.name === name)?.svg
+	}
+
+	const { contactInfo, copyright, socialMedia, subtitle } = response
+
 	return (
 		<footer className='bg-accent'>
 			<div className='mx-auto max-w-screen-2xl px-4 pb-6 pt-10 sm:px-6 lg:px-8 lg:pt-24 text-white'>
 				<div className='flex flex-col justify-between gap-8 sm:flex-row'>
 					<div>
+						{/* Image of the footer */}
 						<div className='flex justify-center text-teal-600 sm:justify-start'>
 							<Image src={FooterLogo} alt='Footer Logo' title='Footer Logo' />
 						</div>
 						<p className='mt-6 max-w-md text-center leading-relaxedsm:max-w-xs sm:text-left'>
-							A pesar de que la empresa es relativamente joven, los valores en
-							los que se basa son sólidos, verdaderos, de otros tiempos.
+							{subtitle}
 						</p>
+						{/* Social media links */}
 						<ul className='mt-8 flex justify-center gap-6 sm:justify-start md:gap-8'>
-							{socialLinks.map(({ name, link, svg }) => (
-								<li key={uuidv4()}>
-									<Link
-										href={link}
-										rel='noreferrer'
-										target='_blank'
-										className='hover:text-gray-400 transition-colors duration-1505'
-									>
-										<span className='sr-only'>{name}</span>
-										{svg}
-									</Link>
-								</li>
-							))}
+							{socialMedia.map(({ id, link, platformName }) => {
+								const svg = findSocialMedia(platformName)
+								return (
+									<li key={id}>
+										<Link
+											href={link}
+											rel='noreferrer'
+											target='_blank'
+											className='hover:text-gray-400 transition-colors duration-1505'
+										>
+											<span className='sr-only'>{platformName}</span>
+											{svg}
+										</Link>
+									</li>
+								)
+							})}
 						</ul>
 					</div>
 
+					{/* Footer links */}
 					<div className='flex flex-col'>
 						<div className='flex gap-6'>
 							<div className='text-center sm:text-left grid place-content-center'>
 								<p className='text-lg font-medium'>Servicios</p>
 								<ul className='mt-8 space-y-4 text-sm'>
-									{footerLinks.services.map(({ name, href }) => (
+									{footerLinks.map(({ name, href }) => (
 										<li key={name}>
 											<Link
 												className='hover:text-gray-400 transition-colors duration-150'
@@ -66,21 +81,25 @@ const Footer = (): JSX.Element => {
 							<div className='text-center sm:text-left grid place-content-center'>
 								<p className='text-lg font-medium'>Contacto</p>
 								<ul className='mt-8 space-y-4 text-sm'>
-									{footerLinks.contact.map(({ name, href }) => (
-										<li key={uuidv4()}>
-											<Link
-												className='hover:text-gray-400 transition-colors duration-150'
-												target='_blank'
-												rel='noreferrer'
-												href={href}
-											>
-												<span className='flex-1'>{name}</span>
-											</Link>
-										</li>
-									))}
+									{Object.entries(contactInfo).map(([key, value]) => {
+										const link =
+											key === 'email' ? `mailto:${value}` : `tel:${value}`
+										return (
+											<li key={uuidv4()}>
+												<Link
+													className='hover:text-gray-400 transition-colors duration-150'
+													href={link}
+												>
+													{value}
+												</Link>
+											</li>
+										)
+									})}
 								</ul>
 							</div>
 						</div>
+
+						{/* Badges */}
 						<ul className='flex gap-3 mt-5'>
 							{badges.map(({ src, alt, title }) => (
 								<li key={uuidv4()}>
@@ -100,12 +119,10 @@ const Footer = (): JSX.Element => {
 
 				<div className='mt-12 pt-6'>
 					<div className='text-center border-b border-white/20 pb-2'>
-						<small className='text-xs md:text-sm'>
-							© 2018 | P.IVA 03118210982 - Registro delle imprese di Brescia -
-							REA: BS-506532 - Capitale sociale i.v.: 10.000,00 - Pec:
-							livinggiardini@legalmail.it
-						</small>
+						<small className='text-xs md:text-sm'>{copyright}</small>
 					</div>
+
+					{/* Designer and developer */}
 					<div className='text-center pt-2 text-gray-300'>
 						<small className='text-xs'>
 							Created with <span className='text-red-500'>❤️</span>, powered by{' '}
