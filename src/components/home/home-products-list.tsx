3f2a4c4d763367b0d9c0863f2d7a36f3e9b30253
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-05 21:31:58 +0200

    feat: Add Sanity Client Fetching

--- src/components/home/home-products-list.tsx
+++ src/components/home/home-products-list.tsx
@@ -4,7 +4,7 @@
 import React, { useEffect, useState } from 'react'
 
 // Type imports
-import type { CategoriesList, Product } from '@/types'
+import type { Product } from '@/types'
 
 // UI component imports
 import { Skeleton } from '@/components/ui/skeleton'
@@ -20,82 +20,69 @@ import { categoriesList } from '@/constants/site-data'
 
 // External Libraries Imports
 import { v4 as uuidv4 } from 'uuid'
-import { sanityClient } from '@sanity-studio/lib/client'
 
-import { allProducts, categories } from '@/lib/queries'
+// Queries Imports
+import { sanityClient } from '@sanity-studio/lib/client'
+import { allProducts } from '@/lib/queries'
 
 /**
  * Renders a list of products based on the selected category.
  *
  * @return {JSX.Element} The JSX element containing the list of products.
  */
-const HomeProductsList = (): JSX.Element => {
-	const [allCategories, setAllCategories] = useState<string[]>([])
-	const [activeCategory, setActiveCategory] = useState<string | undefined>(
-		undefined,
-	)
-	const [products, setProducts] = useState<Product[] | null>(null)
+const HomeProductsList = React.memo(
+	({ categories }: { categories: string[] }): JSX.Element => {
+		// products: The list of products that will be rendered & activeCategory: The currently selected category.
+		const [products, setProducts] = useState<Product[] | null>(null)
+		const [activeCategory, setActiveCategory] = useState<string | undefined>(
+			categories[0],
+		)
 
-	useEffect(() => {
-		const getCategories = async () => {
-			const response: CategoriesList[] = await sanityClient.fetch(categories)
-			const filterCategories = response
-				.map((category) => category.categoria)
-				.filter(
-					(category, index, array) =>
-						category && array.indexOf(category) === index,
-				)
+		// Fetching of data from sanity as part of client side rendering
+		useEffect(() => {
+			sanityClient.fetch(allProducts).then((response: Product[]) => {
+				setProducts(response)
+			})
+		}, [])
 
-			filterCategories.unshift('Todos')
-
-			setAllCategories(filterCategories as string[])
-			setActiveCategory(filterCategories[0] as string)
+		// Rendering of the list of products based on the selected category
+		if (!products) {
+			return <HomeProductsListSkeleton />
 		}
 
-		const getProductsByCategory = async () => {
-			const response: Product[] = await sanityClient.fetch(allProducts)
-			setProducts(response)
-		}
+		return (
+			<section
+				id='home-products-list'
+				className='mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 flex flex-col gap-7'
+			>
+				<HomeProductsListHeader
+					setActiveCategory={setActiveCategory}
+					activeCategory={activeCategory}
+					categories={categories}
+				/>
 
-		getCategories()
-		getProductsByCategory()
-	}, [])
-
-	if (!products) {
-		return <HomeProductsListSkeleton />
-	}
-
-	return (
-		<section
-			id='home-products-list'
-			className='mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 flex flex-col gap-7'
-		>
-			<HomeProductsListHeader
-				setActiveCategory={setActiveCategory}
-				activeCategory={activeCategory}
-				categories={allCategories}
-			/>
-
-			{activeCategory === 'Todos' ? (
-				<ul className='w-full grid content-center grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-6 2xl:gap-10'>
-					{products.slice(0, 8).map((product: Product, index: number) => (
-						<ProductCard key={product.id} product={product} index={index} />
-					))}
-				</ul>
-			) : (
 				<ul className='w-full grid content-center grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-6 2xl:gap-10'>
 					{products
-						.filter((product: Product) => product.categoria === activeCategory)
+						.filter((product: Product) =>
+							activeCategory === 'Todos'
+								? true
+								: product.categoria === activeCategory,
+						)
 						.slice(0, 8)
 						.map((product: Product, index: number) => (
 							<ProductCard key={product.id} product={product} index={index} />
 						))}
 				</ul>
-			)}
-		</section>
-	)
-}
+			</section>
+		)
+	},
+)
 
+/**
+ * Renders the Header of the home products list. using React.useMemo for not re-rendering
+ *
+ * @return {JSX.Element} the Header Component with the filter logic
+ */
 const HomeProductsListHeader = React.memo(
 	({
 		setActiveCategory,
@@ -108,7 +95,7 @@ const HomeProductsListHeader = React.memo(
 	}) => {
 		return (
 			<div className='flex justify-center lg:justify-start items-center flex-wrap gap-10'>
-				{categories.map((category: string) => (
+				{categories.map((category) => (
 					<button
 						type='button'
 						key={uuidv4()}
