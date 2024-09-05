47f2fd00707d0de56e7dc6b53361a29056a8711a
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-05 21:35:19 +0200

    feat: Add Sanity Client Fetching

--- src/components/products/sidebar.tsx
+++ src/components/products/sidebar.tsx
@@ -2,27 +2,34 @@
 import Link from 'next/link'
 
 // Fetcher function
-import { capitalizeFirstLetter } from '@/lib/utils'
-
-// Data imports
-import { categoriesList } from '@/constants/site-data'
+import { capitalizeFirstLetter, categoriesFilter } from '@/lib/utils'
 
 // External Libraies Imports
 import { v4 as uuidv4 } from 'uuid'
+
+// Quiries Imports
+import { sanityClient } from '@sanity-studio/lib/client'
+import type { CategoriesList } from '@/types'
+
+// Utils Imports
+import { categories } from '@/lib/queries'
 
 /**
  * Asynchronously fetches categories from the Fake Store API and renders a sidebar with category links.
  *
  * @param {string | undefined} categoryPath - The category path to display.
- * @return {JSX.Element} The sidebar component with category links.
+ * @return {Promise<JSX.Element>} The sidebar component with category links.
  */
-const ProductsSidebar = ({
+const ProductsSidebar = async ({
 	categoryPath,
-}: { categoryPath: string | undefined }): JSX.Element => {
+}: { categoryPath: string | undefined }): Promise<JSX.Element> => {
+	const response: CategoriesList[] = await sanityClient.fetch(categories)
+	const filterCategories = categoriesFilter(response)
+
 	return (
 		<aside className='flex-[20%] sticky top-5 h-fit hidden md:block'>
 			<ul>
-				{['Todos', ...categoriesList].map((category: string) => {
+				{filterCategories.map((category: string) => {
 					return (
 						<li
 							key={uuidv4()}
@@ -35,7 +42,7 @@ const ProductsSidebar = ({
 						>
 							<Link
 								href={
-									categoryPath === undefined && category === 'Todos'
+									category === 'Todos'
 										? '/products'
 										: `/products?category=${category}`
 								}
