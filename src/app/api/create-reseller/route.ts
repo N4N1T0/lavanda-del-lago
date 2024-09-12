d727745f6369755d4e42eb97bd24824cf83929fd
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-12 22:57:42 +0200

    refactor: Empty Reseller Endpoint

--- src/app/api/create-reseller/route.ts
+++ src/app/api/create-reseller/route.ts
@@ -1,1 +1,8 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
 // TODO Make an API endpoint to create a new reseller
+export async function GET(req: NextRequest) {
+	return NextResponse.json({
+		message: 'Hello World',
+	})
+}
