e5e2e94129b3c95e6c7e4629a982b70801c9b401
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-16 07:50:14 +0200

    refactro: Profile Layout Change

---
+++ src/app/(main-layout)/(profiles)/profile/[id]/page.tsx
@@ -0,0 +1,48 @@
+// Project Components Imports
+import { UserInfoCard } from '@/components/profile/info-card'
+
+// Queries Imports
+import { userById } from '@/lib/queries'
+import { sanityClientRead } from '@sanity-studio/lib/client'
+
+import NewItems from '@/components/profile/new-items'
+import { PastPurchasesCard } from '@/components/profile/past-purchase'
+import { getMostUsedCategory } from '@/lib/utils'
+// Types Imports
+import type { User } from '@/types'
+
+/**
+ * Renders the user profile page.
+ *
+ * @param {Object} params - The parameters for the function.
+ * @param {string} params.id - The ID of the user.
+ * @return {Promise<JSX.Element>} The JSX element representing the user profile page.
+ */
+const UserProfilePage = async ({
+	params,
+}: { params: { id: string } }): Promise<JSX.Element> => {
+	// Fetch user data
+	const response: User = await sanityClientRead.fetch(userById(params.id))
+
+	// Get most used category for the reselling
+	const mostUsedCategory = getMostUsedCategory(response.pastPurchases)
+
+	return (
+		<div className='container mx-auto p-4'>
+			<h1 className='text-3xl font-bold mb-6 uppercase text-accent'>
+				Perfil de Usario
+			</h1>
+			<div className='flex flex-col md:flex-row gap-6'>
+				<div className='w-full md:w-1/4 space-y-6'>
+					<UserInfoCard user={response} />
+					<NewItems category={mostUsedCategory || 'Hogar y ambiente'} />
+				</div>
+				<div className='w-full md:w-3/4'>
+					<PastPurchasesCard purchases={response.pastPurchases} />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default UserProfilePage
---
+++ src/app/(main-layout)/(profiles)/reseller/[id]/page.tsx
@@ -0,0 +1,31 @@
+import ResellerLayout from '@/components/profile/reseller-layout'
+import { userById, allProducts } from '@/lib/queries'
+import type { Product, User } from '@/types'
+import { sanityClientRead } from '@sanity-studio/lib/client'
+
+const ResellerPage = async ({ params }: { params: { id: string } }) => {
+	// Fetch user data
+	const response: User = await sanityClientRead.fetch(userById(params.id))
+
+	// Fetch products
+	const products: Product[] = await sanityClientRead.fetch(allProducts)
+
+	if (response.reseller !== true) {
+		return (
+			<div className='container mx-auto p-4 uppercase text-red-500 text-center text-3xl py-10'>
+				No tienes permiso para usar la plataforma de Revendedor
+			</div>
+		)
+	}
+
+	return (
+		<div className='container mx-auto p-4'>
+			<h1 className='text-3xl font-bold mb-6 uppercase text-accent'>
+				Perfil de Revendedor
+			</h1>
+			<ResellerLayout user={response} products={products} />
+		</div>
+	)
+}
+
+export default ResellerPage
