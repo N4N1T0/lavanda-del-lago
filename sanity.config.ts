3bd37914e7250774e0e645baac073bb8d965ebed
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-23 17:49:50 +0200

    feat: Sanity Structure Change

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,24 +4,27 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import { apiVersion, dataset, projectId } from './sanity/env'
+import { schema } from './sanity/schema'
+import { lavandaCMSStructure } from '@sanity-studio/lib/structure'
 
 export default defineConfig({
-  basePath: '/studio',
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
-  schema,
-  plugins: [
-    structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
+	basePath: '/studio',
+	projectId,
+	dataset,
+	// Add and edit the content schema in the './sanity/schema' folder
+	schema,
+	plugins: [
+		structureTool({
+			structure: lavandaCMSStructure,
+		}),
+		// Vision is a tool that lets you query your content with GROQ in the studio
+		// https://www.sanity.io/docs/the-vision-plugin
+		visionTool({ defaultApiVersion: apiVersion }),
+	],
 })
