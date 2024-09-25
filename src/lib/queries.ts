247e8a52476aa559958cb2d1551cebecc31c6069
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-25 13:34:16 +0200

    feat: Change the GROQ Query style

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,67 +1,6 @@
 import { groq } from 'next-sanity'
 
-// SANITY QUERIES
-export const featuredBlogArticles = groq`
-*[_type == "blog-articles" && featured]{
-  categories,
-  "image": mainImage.asset->url,
-  title,
-  description,
-  author->{
-    name
-  },
-  "id":_id,
-  "slug": slug.current,
-  "createdAt":_createdAt
-}`
-
-export const allBlogArticles = groq`
-*[_type == "blog-articles"]{
-  categories,
-  "image": mainImage.asset->url,
-  title,
-  description,
-  author->{
-    name
-  },
-  "id":_id,
-  "slug": slug.current,
-  "createdAt":_createdAt
-}`
-
-export const blogArticleById = (slug: string) => {
-	return groq`
-  *[_type == "blog-articles" && slug.current == "${slug}"][0]{
-  categories,
-  "image": mainImage.asset->url,
-  title,
-  description,
-  author->{
-    name
-  },
-  content,
-  "createdAt":_createdAt
-}
-  `
-}
-
-export const relatedArticulesByCategory = (category: string) => {
-	return groq`
-  *[_type == "blog-articles" && "${category}" in categories]{
-  categories,
-  "image": mainImage.asset->url,
-  title,
-  description,
-  author->{
-    name
-  },
-  "id":_id,
-  "slug": slug.current,
-  "createdAt":_createdAt
-}
-  `
-}
-
+// PAGES
 export const aboutUsPage = groq`
 *[_type == "aboutPage"][0]{
   title,
@@ -118,80 +57,6 @@ export const homePage = groq`
   }
 }[0]
 `
-export const events = groq`
-*[_type == "events"]{
-  "id": _id,
-  date,
-  title,
-  "image": image.asset->url,
-  description,
-  urls[]{
-    "id": _key,
-    calendarName,
-    calendarUrl
-  }
-}
-`
-export const categories = groq`
-*[_type == "product"]{
-  categoria
-} | order(categoria)
-`
-
-export const allProducts = groq`
-*[_type == "product"]{
-  "id": _id,
-  nombre,
-  descripcion,
-  precio,
-  "image": fotoPrincipal.asset->url,
-  categoria,
-  stock,
-  "createdAt": _createdAt
-}
-`
-
-export const productByName = (name: string) => {
-	return groq`
-  *[_type == "product" && nombre == "${name}"]{
-  "id": _id,
-  nombre,
-  descripcion,
-  precio,
-  "image": fotoPrincipal.asset->url,
-  categoria,
-  stock,
-  "createdAt": _createdAt,
-  usabilidad,
-  subcategoria,
-  codigoReferencia,
-  certificacion,
-  medidas,
-  codigoBarras,
-  slogan,
-  "fichaTecnica": fichaTecnica.asset->url,
-  fotosVarias[]{
-    "image": asset->url,
-    "key": _key
-  }
-}[0]
-  `
-}
-
-export const productsByCategory = (category: string) => {
-	return groq`
-  *[_type == "product" && categoria == "${category}"]{
-  "id": _id,
-  nombre,
-  descripcion,
-  precio,
-  "image": fotoPrincipal.asset->url,
-  categoria,
-  stock,
-  "createdAt": _createdAt
-}
-  `
-}
 
 export const footer = groq`
 *[_type == "footer"]{
@@ -206,54 +71,52 @@ export const footer = groq`
 }[0]
 `
 
-export const policiesPages = (name: 'cookie' | 'privacy' | 'sales') => {
-	switch (name) {
-		case 'cookie':
-			return groq`*[_type == "cookiePolicy"]{
-        title,
-        description,
-        content
-      }[0]
-      `
-		case 'privacy':
-			return groq`*[_type == "privacyPolicy"]{
-        title,
-        description,
-        content
-      }[0]
-      `
-		case 'sales':
-			return groq`*[_type == "salesPolicy"]{
-        title,
-        description,
-        content
-      }[0]
-      `
-	}
-}
+export const cookiePolicy = groq`
+  *[_type == "cookiePolicy"]{
+    title,
+    description,
+    content
+  }[0]
+`
 
-export const errorPages = (name: 'error' | 'not-found') => {
-	return name === 'error'
-		? groq`*[_type == "errorPage"]{
-      title,
-      description,
-      digest,
-      "imageUrl": image.asset->url,
-      contacts[]{
-        label,
-        link
-      }
-    }[0]
-    `
-		: groq`*[_type == "notFoundPage"]{
-      title,
-      description,
-      digest,
-      "imageUrl": image.asset->url,
-      links
-    }[0]
-    `
-}
+export const privacyPolicy = groq`
+  *[_type == "privacyPolicy"]{
+    title,
+    description,
+    content
+  }[0]
+`
+
+export const salesPolicy = groq`
+  *[_type == "salesPolicy"]{
+    title,
+    description,
+    content
+  }[0]
+`
+
+export const errorPage = groq`
+  *[_type == "errorPage"]{
+    title,
+    description,
+    digest,
+    "imageUrl": image.asset->url,
+    contacts[] {
+      label,
+      link
+    }
+  }[0]
+`
+
+export const notFoundPage = groq`
+  *[_type == "notFoundPage"]{
+    title,
+    description,
+    digest,
+    "imageUrl": image.asset->url,
+    links
+  }[0]
+`
 
 export const prefooter = groq`*[_type == "prefooter"]{
   title,
@@ -286,31 +149,190 @@ export const seo = groq`*[_type == "seoMetatags"]{
 }[0]
 `
 
-export const userById = (id: string) => {
-	return groq`
-  *[_type == "user" && _id == "${id}"]{
-  "id": _id,
-  email,
-  image,
-  name,
-  phone,
-  address,
-  reseller,
-  discount,
-  "pastPurchases": *[_type == "purchase" && userEmail._ref == ^._id]{
-    "id": _id,
-    products[]->{
-      "id": _id,
-      nombre,
-      "image": fotoPrincipal.asset->url,
-      categoria
-    },
-    totalAmount,
-    purchaseDate,
-    paymentMethod,
-    status,
-    reseller,
-  }
+export const jusAFLower = groq`*[_type == "justAFlower"]{
+   "mainImage": mainImage.asset->url,
+   "secondaryImage": secondaryImage.asset->url,
+   text,
+   secondaryText,
+   quote,
+   title
+ }[0]
+`
+
+export const property = groq`*[_type == "property"]{
+  title,
+  content,
+  "product": featuredProduct->{
+     nombre,
+    descripcion,
+    precio,
+    "image": fotoPrincipal.asset->url,
+    categoria,
+    stock,
+  },
+  "featuredImage": featuredImage.asset->url
 }[0]
-  `
+`
+
+// BLOG
+export const featuredBlogArticles = groq`
+*[_type == "blog-articles" && featured]{
+  categories,
+  "image": mainImage.asset->url,
+  title,
+  description,
+  author->{
+    name
+  },
+  "id":_id,
+  "slug": slug.current,
+  "createdAt":_createdAt
+}`
+
+export const allBlogArticles = groq`
+*[_type == "blog-articles"]{
+  categories,
+  "image": mainImage.asset->url,
+  title,
+  description,
+  author->{
+    name
+  },
+  "id":_id,
+  "slug": slug.current,
+  "createdAt":_createdAt
+}`
+
+export const blogArticleById = groq`
+  *[_type == "blog-articles" && slug.current == $slug][0]{
+    categories,
+    "image": mainImage.asset->url,
+    title,
+    description,
+    author->{
+      name
+    },
+    content,
+    "createdAt":_createdAt
+  }
+`
+
+export const relatedArticlesByCategory = groq`
+  *[_type == "blog-articles" && $category in categories]{
+    categories,
+    "image": mainImage.asset->url,
+    title,
+    description,
+    author->{
+      name
+    },
+    "id":_id,
+    "slug": slug.current,
+    "createdAt":_createdAt
+  }
+`
+
+// PRODUCTS
+export const categories = groq`
+*[_type == "product"]{
+  categoria
+} | order(categoria)
+`
+
+export const allProducts = groq`
+*[_type == "product"]{
+  "id": _id,
+  nombre,
+  descripcion,
+  precio,
+  "image": fotoPrincipal.asset->url,
+  categoria,
+  stock,
+  "createdAt": _createdAt
 }
+`
+
+export const productByName = groq`
+  *[_type == "product" && nombre == $name]{
+    "id": _id,
+    nombre,
+    descripcion,
+    precio,
+    "image": fotoPrincipal.asset->url,
+    categoria,
+    stock,
+    "createdAt": _createdAt,
+    usabilidad,
+    subcategoria,
+    codigoReferencia,
+    certificacion,
+    medidas,
+    codigoBarras,
+    slogan,
+    "fichaTecnica": fichaTecnica.asset->url,
+    fotosVarias[] {
+      "image": asset->url,
+      "key": _key
+    }
+  }[0]
+`
+
+export const productsByCategory = groq`
+  *[_type == "product" && categoria == $category]{
+    "id": _id,
+    nombre,
+    descripcion,
+    precio,
+    "image": fotoPrincipal.asset->url,
+    categoria,
+    stock,
+    "createdAt": _createdAt
+  }
+`
+
+// AUTH
+export const userById = groq`
+  *[_type == "user" && _id == $id][0] {
+    "id": _id,
+    email,
+    image,
+    name,
+    phone,
+    address,
+    reseller,
+    discount,
+    "pastPurchases": *[_type == "purchase" && userEmail._ref == ^._id] {
+      "id": _id,
+      products[]{
+        product->{
+        "id": _id,
+        nombre,
+        "image": fotoPrincipal.asset->url,
+        categoria
+      },
+        quantity
+      },
+      totalAmount,
+      purchaseDate,
+      paymentMethod,
+      status,
+      reseller
+    }
+  }
+`
+
+// MISC
+export const events = groq`
+*[_type == "events"]{
+  "id": _id,
+  date,
+  title,
+  "image": image.asset->url,
+  description,
+  urls[]{
+    "id": _key,
+    calendarName,
+    calendarUrl
+  }
+}
+`
